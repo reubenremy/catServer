bc0ef2cc6b117cfa404fa5c223ab1467c2bd3059
Reuben Remy <reubenremy@gmail.com>
2022-10-31 06:43:11 -0500

    .

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+
+app.use("/", (req, res) => {
+    res.send('Hello World')
+})
+
+const port = 8080 || process.env.PORT
+
+app.listen(port, () => {
+    console.log('hello world from port >>> ' + port)
+})
