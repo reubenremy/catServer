19ee958bd3c195f811a7b93696b5ca3b148b1715
Reuben Remy <reubenremy@gmail.com>
2022-11-03 00:12:24 -0500

    .

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
 const express = require('express')
 const cors = require('cors')
 
-const db = require('./app/database')
+const User = require('./app/models/user.model')
 
 const app = express()
 
 app.use(cors())
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const testDB = async () => {
-    const response = await new Promise((resolve, reject) => {
-        db.query('show tables;', async(err, res) => {
-            console.log(res, err)
-            if (err) reject(err)
-            resolve(res)
-        })
-    })
-    console.log("response is", response)
-}
-
-testDB()
-
 app.use("/", (req, res) => {
-    res.send('Hello World 2')
+    res.send(User.findById(1))
 })
 
 const port = 8080 || process.env.PORT
